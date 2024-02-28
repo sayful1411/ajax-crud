098d1870316692f421a10463b267731d36514339
sayful <mdsayfulislam1411@gmail.com>
2024-02-28 11:56:41 +0600

    setup admin blade template

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title> @yield('title') </title>
+
+        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+        <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="sb-nav-fixed">
+
+        @include('partials.admin.header')
+
+        <div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                
+                @include('partials.admin.sidebar')
+
+            </div>
+            <div id="layoutSidenav_content">
+
+                <main>
+                    @yield('content')
+                </main>
+                
+                @include('partials.admin.footer')
+
+            </div>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="{{ asset('js/scripts.js') }}"></script>
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js"></script>
+        <script src="{{ asset('js/datatables-simple-demo.js') }}"></script>
+    </body>
+    
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
@@ -37,3 +38,5 @@ Route::middleware('auth')->group(function () {
 
     Route::post('/logout', [LoginController::class, 'destroy'])->name('logout');
 });
+
+Route::get('/admin/login', [AdminController::class, 'index'])->name('admin.login');
