1731ef8b6237878a4d9fa42c99c3fa990371f08f
sayful <mdsayfulislam1411@gmail.com>
2024-02-28 10:08:10 +0600

    setup login blade

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.guest')
+
+@section('title', 'Login | ' . config('app.name'))
+
+@section('content')
+    <div class="col-md-6 mx-auto">
+        <form>
+            <div class="form-group">
+              <label for="email">Email address</label>
+              <input type="email" class="form-control" id="email" name="email" placeholder="Enter email">
+            </div>
+            <div class="form-group">
+              <label for="password">Password</label>
+              <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+            </div>
+            <div class="form-group form-check">
+              <input type="checkbox" name="remember" class="form-check-input" id="remember">
+              <label class="form-check-label" for="remember">Remember me</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Login</button>
+          </form>
+    </div>
+@endsection
---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title> @yield('title') </title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" />
+</head>
+
+<body>
+    <div class="container mx-auto">
+        <div class="col-md-12">
+            <div class="container">
+                <header class="d-flex justify-content-center py-3">
+                    <ul class="nav nav-pills">
+                        <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Register</a></li>
+                    </ul>
+                </header>
+            </div>
+        </div>
+
+        <div class="container mx-auto">
+            @yield('content')
+        </div>
+    </div>
+</body>
+
+</html>
